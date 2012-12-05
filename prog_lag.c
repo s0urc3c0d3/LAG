5a92bb2d190bf8a04dad6a4ab6552f478eda5b61
Grzegorz Dwornicki <g.r.d0110@gmail.com>
2012-12-05 01:32:43 +0100

    dodalem obsluge kdumpa - pozwali zdebugowac czemu podwujne wywolanie pick_task_next_fair powoduje kernel panic

--- prog_lag.c
+++ prog_lag.c
@@ -8,26 +8,10 @@ int main(int argc, char *argv[])
 {
 	int lag_file = open("/dev/lag",O_RDWR);
 	struct lag_request req,res;
-	req.REQID=0;
-	req.pid=1812;
+	req.REQID=1;
+	req.pid=1626;
 	req.status=0;
 	write(lag_file,&req,sizeof(struct lag_request));
-	req.status=0;
-	read(lag_file,&res,sizeof(struct lag_request));
-	printf("\n status: %i   pid %i",res.status,res.pid);
-	printf("\n pauzuje proces...");
-	req.REQID=1;
-	req.status=4;
-	write(lag_file,&req,sizeof(struct lag_request));
-	read(lag_file,&res,sizeof(struct lag_request));
-	printf("\n status: %i   pid %i",res.status,res.pid);
-	char g;
-	scanf("%s",&g);
-	req.REQID=1;
-	req.status=1;
-	write(lag_file,&req,sizeof(struct lag_request));
-	read(lag_file,&res,sizeof(struct lag_request));
-	printf("\n status: %i   pid %i",res.status,res.pid);
 	close(lag_file);
 	return 0;
 }
