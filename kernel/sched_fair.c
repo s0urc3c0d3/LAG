1c12a5d2ed0b110e28a93b329a3501a1c7af7c1c
root <root@debian.(none)>
2012-12-18 23:20:16 +0100

    Okazuje sie, ze kwant czasu to nie vruntime ale inne pole (patrz funckje put_prev_entity oraz equeue_task i dequque_task

--- kernel/sched_fair.c
+++ kernel/sched_fair.c
@@ -1765,7 +1765,8 @@ static struct task_struct *pick_next_task_lag(struct rq *rq)
 		if (tsk!=NULL && wq->tsk->pid==tsk->pid) 
 	//asm("#a");
 		{
-			tsk->se.vruntime=0x000000000fffffff;
+			tsk->se.exec_start=0;
+			printk(KERN_DEBUG "obecny przez zmiana tsk %i %llu  ",tsk->pid,(unsigned long long)tsk->se.vruntime);
 			put_prev_task_fair(rq,tsk);
 			printk(KERN_DEBUG "obecny tsk %i %llu  ",tsk->pid,(unsigned long long)tsk->se.vruntime);
 	//asm("#b");
