fb81ab356cb5c28ff3d7708cba09b13d0b4ac7f5
root <root@debian.(none)>
2013-01-03 17:16:46 +0100

    porpawiony blad w scenariuszu... musialo to jakis nie zostac wcommitowane

--- prog_lag.c
+++ prog_lag.c
@@ -10,7 +10,8 @@ int main(int argc, char *argv[])
 	struct lag_request req,res;
 	if (argc < 3) printf("za malo argumentow!\n");
 	req.REQID=atoi(argv[2]);
-	req.pid=atoi(argv[1](;
+	req.pid=atoi(argv[1]);
+	printf("%i %i\n",req.pid,req.REQID);
 	req.status=0;
 	write(lag_file,&req,sizeof(struct lag_request));
 	close(lag_file);
--- tests/00-scenario1.sh
+++ tests/00-scenario1.sh
@@ -14,7 +14,7 @@ mkdir tmp/00scenario1 -p
 WORKDIR=`pwd`/tmp/00scenario1
 PROCS=""
 
-for i in `ps ax | grep test_prog | tail -n $NPROC | awk '{print $1}'`; do PROCS="$PROCS $i"; done
+for i in `ps ax | grep test_prog | grep -v 'grep' | tail -n $NPROC | awk ' {print $1}'` ; do PROCS="$PROCS $i"; done
 
 echo $PROCS > $WORKDIR/procs.txt
 LICZNIK=0
@@ -24,7 +24,7 @@ do
 	PROC=$(echo $i | awk 'BEGIN {FS="."} { print $1}')
 	OP=$(echo $i | awk 'BEGIN {FS="."} { print $2}')
 	mkdir $WORKDIR/$LICZNIK
-	nPROC=$(echo $PROCS | awk "{print $PROC}")
+	nPROC=$(echo $PROCS | awk "{print $"$PROC"}")
 	./prog_lag $nPROC $OP
 	sleep 2
 	dmesg > $WORKDIR/$LICZNIK/dmesg.txt
